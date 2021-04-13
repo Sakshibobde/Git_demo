d78889949b084b8912c4212cb465b5c38013a3f1
sakshibobde <sakshibo17@gmail.com>
2021-04-13 19:51:51 +0530

    adding eclips assignment

---
+++ eclips_test/src/eclips_test/Test_Class.java
@@ -0,0 +1,15 @@
+package eclips_test;
+
+public class Test_Class {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Sakshi Bobde");
+		
+
+		int X=10;
+		int Y=50;
+		System.out.println(X+Y);
+
+	}
+
+}
---
+++ eclips_test/src/module-info.java
@@ -0,0 +1,2 @@
+module eclips_test {
+}
